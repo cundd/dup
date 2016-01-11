eac40ccc3200cb24ba82b2e518d7410a84ce4cd2
Daniel Corn <d@cundd.net>
2016-01-11 17:54:07 +0100

    Updated DUP environment variable name

--- dup/scripts/unprivileged_always/bash-setup.sh
+++ dup/scripts/unprivileged_always/bash-setup.sh
@@ -10,7 +10,7 @@ source "$DUP_LIB_PATH";
 function run() {
     if [[ "$SETUP_BASH" == "true" ]]; then
         duplib::add_string_to_file_if_not_found "export DUP_LIB_PATH=$DUP_LIB_PATH" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DUP_DOCUMENT_ROOT=$(duplib::get_vhost_document_root)" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$(duplib::get_vhost_document_root)" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
