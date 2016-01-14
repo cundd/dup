2d6b0b69376d4b96aacd0ed5b0960068fe827832
Daniel Corn <d@cundd.net>
2016-01-14 12:04:45 +0100

    Fixed an error message

--- dup/scripts/lib/includes/utils.sh
+++ dup/scripts/lib/includes/utils.sh
@@ -22,7 +22,7 @@ function duplib::add_string_to_file_if_not_found() {
 # Helpers
 # --------------------------------------------------------
 function duplib::get_option_is_set() {
-    if [[ -z ${1+x} ]]; then duplib::error "Missing argument 1 (user@server)"; return 1; fi;
+    if [[ -z ${1+x} ]]; then duplib::error "Missing argument 1 (option)"; return 1; fi;
 
     local option="$1";
     shift;
