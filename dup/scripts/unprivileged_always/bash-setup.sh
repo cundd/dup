e45f3e48e302d36fd3d4125d74024d4da92d0f9c
Daniel Corn <d@cundd.net>
2016-01-26 12:28:18 +0100

    Added mysql host to the bash setup

--- dup/scripts/unprivileged_always/bash-setup.sh
+++ dup/scripts/unprivileged_always/bash-setup.sh
@@ -12,11 +12,11 @@ function run() {
         local vhost_document_root="$(duplib::get_vhost_document_root)";
         duplib::add_string_to_file_if_not_found "export DUP_LIB_PATH=$DUP_LIB_PATH" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$vhost_document_root" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found "export DB_HOST=$DB_HOST" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
         duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
 
         # Change to the vhost document root
         duplib::add_string_to_file_if_not_found "cd $vhost_document_root" "$HOME/.profile";
