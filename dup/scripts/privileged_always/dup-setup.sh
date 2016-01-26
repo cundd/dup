c73831e7281afd52c774321f0d00382f00b8dd3f
Daniel Corn <d@cundd.net>
2016-01-26 11:53:30 +0100

    Fixed a bug in the dup master tool installation

--- dup/scripts/privileged_always/dup-setup.sh
+++ dup/scripts/privileged_always/dup-setup.sh
@@ -7,7 +7,7 @@ BASH_SETUP="${BASH_SETUP:-true}";
 function run() {
     if [[ "$BASH_SETUP" == "true" ]]; then
         if [[ ! -e "/usr/local/bin/dup" ]]; then
-            ln -s "/vagrant/dup/cli" "/usr/local/bin/dup";
+            ln -s "/vagrant/dup/scripts/cli/dup" "/usr/local/bin/dup";
         fi
     fi
 }
