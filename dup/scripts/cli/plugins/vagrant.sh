049babae5a350a8ae88917adb5f88f0d51235fac
Daniel Corn <d@cundd.net>
2016-01-26 11:11:13 +0100

    Added vagrant::hosts_add()
    Added Vagrant plugin command to add a domain name for the configured IP address to the hosts file

--- dup/scripts/cli/plugins/vagrant.sh
+++ dup/scripts/cli/plugins/vagrant.sh
@@ -33,3 +33,16 @@ function dupcli::vagrant::reload() {
     dupcli::_vagrant::check;
     vagrant reload $@;
 }
+
+function dupcli::vagrant::hosts_add() {
+    if [[ -z ${1+x} ]]; then duplib::error "Missing argument 1 (domain.local)"; return 1; fi;
+
+    set -e;
+    if [ ! -w "/etc/hosts" ]; then
+        duplib::error "Hosts file is not writable";
+        exit 1;
+    fi
+
+    echo "Adding $(dupcli::config "vagrant.vm.ip") as $1 to /etc/hosts";
+    echo "$(dupcli::config "vagrant.vm.ip")  $1" >> /etc/hosts;
+}
