51a91b044f97ef471ffdfb2de111cb5b04319bc8
Daniel Corn <d@cundd.net>
2016-01-26 14:29:17 +0100

    Set completion-ignore-case on in BASH setup

--- dup/scripts/unprivileged_always/bash-setup.sh
+++ dup/scripts/unprivileged_always/bash-setup.sh
@@ -7,20 +7,29 @@ BASH_SETUP="${BASH_SETUP:-true}";
 DUP_LIB_PATH="${DUP_LIB_PATH:-$(dirname "$0")/../special/lib.sh}";
 source "$DUP_LIB_PATH";
 
-function run() {
-    if [[ "$BASH_SETUP" == "true" ]]; then
-        local vhost_document_root="$(duplib::get_vhost_document_root)";
-        duplib::add_string_to_file_if_not_found "export DUP_LIB_PATH=$DUP_LIB_PATH" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$vhost_document_root" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DB_HOST=$DB_HOST" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
-        duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
+function configure-completion() {
+    duplib::add_string_to_file_if_not_found "set completion-ignore-case on" "$HOME/.inputrc";
+}
 
-        # Change to the vhost document root
-        duplib::add_string_to_file_if_not_found "cd $vhost_document_root" "$HOME/.profile";
+function configure-environment() {
+    local vhost_document_root="$(duplib::get_vhost_document_root)";
+    duplib::add_string_to_file_if_not_found "export DUP_LIB_PATH=$DUP_LIB_PATH" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$vhost_document_root" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found "export DB_HOST=$DB_HOST" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
+    duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
+
+    # Change to the vhost document root
+    duplib::add_string_to_file_if_not_found "cd $vhost_document_root" "$HOME/.profile";
+}
+
+function main() {
+    if [[ "$BASH_SETUP" == "true" ]]; then
+        configure-environment;
+        configure-completion;
     fi
 }
 
-run $@;
+main $@;
