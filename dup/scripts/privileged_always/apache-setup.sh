1c300655c870d1ee3dabb9cd289d3326fc3513a3
Daniel Corn <d@cundd.net>
2016-01-27 18:10:07 +0100

    Commented out vhost doc root owner modification

--- dup/scripts/privileged_always/apache-setup.sh
+++ dup/scripts/privileged_always/apache-setup.sh
@@ -43,10 +43,10 @@ function prepare_document_root() {
     # Try
     set +e
     if [[ "$(getent group apache)" != "" ]]; then
-        chown apache:apache $documentRoot;
+        #chown apache:apache $documentRoot;
         chmod g+w $documentRoot;
     elif [[ "$(getent group http)" != "" ]]; then
-        chown http:http $documentRoot;
+        #chown http:http $documentRoot;
         chmod g+w $documentRoot;
     else
         duplib::error "Could not detect apache/http group name";
