88afc8ba4f3d4e2ad1c8946ac8ae0acc3ff491af
Daniel Corn <d@cundd.net>
2016-01-28 17:31:52 +0100

    Hide hosts file warning for Vagrant connections

--- dup/scripts/cli/plugins/ssh.sh
+++ dup/scripts/cli/plugins/ssh.sh
@@ -11,7 +11,7 @@ function dupcli::ssh::connect() {
     if [[ $(dupcli::is_guest) == "yes" ]]; then
         duplib::fatal_error "This machine appears to be the guest machine";
     elif [[ -e "$DUP_BASE/.vagrant/machines/default/virtualbox/id" ]]; then
-        dupcli::_ssh::vagrant_connect "$@";
+        dupcli::_ssh::vagrant::connect "$@";
     else
         duplib::fatal_error "No supported way to connect found";
     fi
@@ -25,7 +25,7 @@ function dupcli::ssh::execute() {
     if [[ $(dupcli::is_guest) == "yes" ]]; then
         duplib::fatal_error "This machine appears to be the guest machine";
     elif [[ -e "$DUP_BASE/.vagrant/machines/default/virtualbox/id" ]]; then
-        dupcli::_ssh::vagrant_execute "$@";
+        dupcli::_ssh::vagrant::execute "$@";
     else
         duplib::fatal_error "No supported way to connect found";
     fi
@@ -37,21 +37,30 @@ function dupcli::ssh::exec() {
 
 
 # --------------------------------------------------------
-# Helper methods
+# Vagrant adapter
 # --------------------------------------------------------
-function dupcli::_ssh::get_vagrant_user_and_host() {
+function dupcli::_ssh::vagrant::user_and_host() {
     echo "vagrant@127.0.0.1";
 }
 
-function dupcli::_ssh::vagrant_get_port() {
+function dupcli::_ssh::vagrant::options() {
+    echo "" \
+        "-o Compression=yes -o DSAAuthentication=yes -o LogLevel=FATAL " \
+        "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " \
+        "-o IdentitiesOnly=yes -i $DUP_BASE/.vagrant/machines/default/virtualbox/private_key" \
+        "-o ConnectTimeout=1 -o ConnectionAttempts=1";
+}
+
+function dupcli::_ssh::vagrant::port() {
     VBoxManage showvminfo "$(cat "$DUP_BASE/.vagrant/machines/default/virtualbox/id")" --machinereadable |\
         grep "Forwarding(\d)=\"ssh,tcp,127\.0\.0\.1"|\
         awk -F, '{print $4}';
 }
 
-function dupcli::_ssh::vagrant_connect() {
-    local user_and_server=$(dupcli::_ssh::get_vagrant_user_and_host);
-    local port=$(dupcli::_ssh::vagrant_get_port);
+function dupcli::_ssh::vagrant::connect() {
+    local user_and_server=$(dupcli::_ssh::vagrant::user_and_host);
+    local port=$(dupcli::_ssh::vagrant::port);
+    local options=$(dupcli::_ssh::vagrant::options);
 
     local verbose='';
     if [[ $(duplib::get_option_is_set "-v" $@) == "true" ]]; then
@@ -62,7 +71,7 @@ function dupcli::_ssh::vagrant_connect() {
         verbose="-vvv";
     fi
 
-    ssh $verbose -o ConnectTimeout=1 -o ConnectionAttempts=1 -p "$port" "$user_and_server" || {
+    ssh $verbose $options -p "$port" "$user_and_server" || {
          # Check if a timeout occurred
          if [ $? -eq 255 ]; then
              echo "Try to connect through vagrant binary";
@@ -73,9 +82,10 @@ function dupcli::_ssh::vagrant_connect() {
     }
 }
 
-function dupcli::_ssh::vagrant_execute() {
-    local user_and_server=$(dupcli::_ssh::get_vagrant_user_and_host);
-    local port=$(dupcli::_ssh::vagrant_get_port);
+function dupcli::_ssh::vagrant::execute() {
+    local user_and_server=$(dupcli::_ssh::vagrant::user_and_host);
+    local port=$(dupcli::_ssh::vagrant::port);
+    local options=$(dupcli::_ssh::vagrant::options);
 
     local verbose='';
     if [[ $(duplib::get_option_is_set "-v" $@) == "true" ]]; then
@@ -86,7 +96,7 @@ function dupcli::_ssh::vagrant_execute() {
         verbose="-vvv";
     fi
 
-    ssh $verbose -o ConnectTimeout=1 -o ConnectionAttempts=1 -p "$port" "$user_and_server" "bash -l -c '$@'" || {
+    ssh $verbose $options -p "$port" "$user_and_server" "bash -l -c '$@'" || {
         # Check if a timeout occurred
         if [ $? -eq 255 ]; then
             echo "Try to connect through vagrant binary";
