12d6311b8dc3e53c9f36ad1e6b133689a68830b5
Daniel Corn <d@cundd.net>
2016-02-03 12:38:52 +0100

    Added a check if the DUP_LIB_PATH is valid

--- shell/lib/duplib.sh
+++ shell/lib/duplib.sh
@@ -3,6 +3,10 @@
 # duplib = a bash function library
 
 DUP_LIB_PATH="${DUP_LIB_PATH:-$0}";
+if [ ! -e "$DUP_LIB_PATH" ]; then
+    >&2 echo "Defined duplib path '$DUP_LIB_PATH' not found";
+    exit 1;
+fi
 DUP_LIB_INCLUDES_PATH="${DUP_LIB_INCLUDES_PATH:-$(dirname "$DUP_LIB_PATH")/includes}";
 
 source "$DUP_LIB_INCLUDES_PATH/utils.sh";
