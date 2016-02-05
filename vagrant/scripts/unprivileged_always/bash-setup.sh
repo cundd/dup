d75a3e63d00db1f98d47a9f2c31c753314bf06b3
Daniel Corn <d@cundd.net>
2016-02-05 13:05:19 +0100

    Enhanced the bash setup

--- vagrant/scripts/unprivileged_always/bash-setup.sh
+++ vagrant/scripts/unprivileged_always/bash-setup.sh
@@ -13,17 +13,23 @@ function configure-completion() {
 }
 
 function configure-environment() {
-    local vhost_document_root="$(duplib::get_vhost_document_root)";
     duplib::add_string_to_file_if_not_found "export DUP_LIB_PATH=$DUP_LIB_PATH" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$vhost_document_root" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found "export DB_HOST=$DB_HOST" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
-    duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
 
-    # Change to the vhost document root
-    duplib::add_string_to_file_if_not_found "cd $vhost_document_root" "$HOME/.profile";
+    if [[ ! -z ${DB_HOST+x} ]] && [[ ! -z ${DB_NAME+x} ]] && [[ ! -z ${DB_USERNAME+x} ]] && [[ ! -z ${DB_PASSWORD+x} ]]; then
+        duplib::add_string_to_file_if_not_found "export DB_HOST=$DB_HOST" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found "export DB_NAME=$DB_NAME" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found "export DB_USERNAME=$DB_USERNAME" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found "export DB_PASSWORD=$DB_PASSWORD" "$HOME/.profile";
+        duplib::add_string_to_file_if_not_found 'alias dup-mysql="mysql -h$DB_HOST -u$DB_USERNAME -p$DB_PASSWORD -D$DB_NAME"' "$HOME/.profile";
+    fi
+
+    local vhost_document_root="$(duplib::get_vhost_document_root)";
+    if [[ -e "$vhost_document_root" ]]; then
+        duplib::add_string_to_file_if_not_found "export DUP_VHOST_DOCUMENT_ROOT=$vhost_document_root" "$HOME/.profile";
+
+        # Change to the vhost document root
+        duplib::add_string_to_file_if_not_found "cd $vhost_document_root" "$HOME/.profile";
+    fi
 }
 
 function main() {
