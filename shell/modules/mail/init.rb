3cd91fff3a7554b76490a007ab6b5564064e4947
Daniel Corn <d@cundd.net>
2016-02-15 15:58:11 +0100

    Disabled MailHog port forwarding

--- shell/modules/mail/init.rb
+++ shell/modules/mail/init.rb
@@ -1,9 +1,10 @@
 class Mail
     def initialize(vagrantConfig, moduleConfiguration)
-        # Forward the GUI port for MailHog
-        if moduleConfiguration['mailhog_setup']
-            vagrantConfig.vm.network "forwarded_port", guest: "8025", host: "8025"
-        end
+        ## Example:
+        ## Forward the GUI port for MailHog
+        # if moduleConfiguration['mailhog_setup']
+        #     vagrantConfig.vm.network "forwarded_port", guest: "8025", host: "8025"
+        # end
     end
 
 end
