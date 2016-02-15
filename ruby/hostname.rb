3292f989523bdd9b1920c230addd4253ca0980f1
Daniel Corn <d@cundd.net>
2016-02-15 10:47:48 +0100

    Set hostname automatically per default
    Use the project.dev.url configuration as hostname if defined

--- ruby/hostname.rb
+++ ruby/hostname.rb
@@ -1,7 +1,17 @@
+require 'uri'
+
 def configureAutomaticHostname(config, path)
     hostname = getConfig()['vagrant']['vm']['hostname']
     if hostname == 'automatic'
-        config.vm.hostname = File.basename(File.expand_path(path))
+        projectUrl = getConfig()['project']['dev']['url']
+        if projectUrl
+            config.vm.hostname = projectUrl
+        else
+            folderName = File.basename(File.expand_path(path))
+            if folderName.scan(URI.regexp)
+                config.vm.hostname = folderName
+            end
+        end
     elsif hostname
         config.vm.hostname = hostname
     end
